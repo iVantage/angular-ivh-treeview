cc79c2661e2b1421f5c58fafa77534bfee3d8829
Justin <jus.russell@gmail.com>
2014-06-20 16:11:00 -0400

    test: Add the start of some tests

---
+++ test/spec/directives/ivh-treeview.js
@@ -0,0 +1,77 @@
+/*global jQuery, describe, beforeEach, afterEach, it, module, inject, expect */
+
+describe('Directive ivhTreeview', function() {
+  'use strict';
+
+  var ng = angular
+    , $ = jQuery;
+
+  var scope, compile;
+
+  beforeEach(module('ivh.treeview'));
+
+  var tplBasic = '<div ivh-treeview="bag1"></div>';
+
+  var tplFilter = [
+    '<div',
+      'ivh-treeview="bag1"',
+      'ivh-treeview-filter="filter:myFilter"',
+      '></div>'
+  ].join('\n');
+
+  beforeEach(inject(function($rootScope, $compile, $timeout) {
+    scope = $rootScope.$new();
+    scope.bag1 = [{
+      label: 'top hat',
+      children: [{
+        label: 'flat cap'
+      },{
+        label: 'fedora',
+        children: [{label: 'gatsby'}]
+      }]
+    },{
+      label: 'baseball'
+    }];
+
+    compile = function(tpl, scp) {
+      var $el = $compile(ng.element(tpl))(scp);
+      scp.$apply();
+      $timeout.flush();
+      return $el;
+    };
+  }));
+
+  describe('basics', function() {
+    var $el;
+
+    beforeEach(function() {
+      $el = compile(tplBasic, scope);
+    });
+
+    it('should create a tree layout', function() {
+      expect($el
+        .find('ul').eq(0) // Entire tree
+        .find('ul').eq(0) // tree.children (flat cap tree)
+        .find('ul').eq(0) // tree.children.children (gasby tree)
+        .find('li').length
+      ).toBe(1);
+    });
+
+    /**
+     * @todo Collapsing/Expanding
+     */
+  });
+
+  describe('filtering', function() {
+    var $el;
+
+    beforeEach(function() {
+      scope.myFilter = 'baseball';
+      $el = compile(tplFilter, scope);
+    });
+
+    it('should hide filtered out nodes', function() {
+      expect($el.find('li').length).toBe(3);
+    });
+  });
+});
