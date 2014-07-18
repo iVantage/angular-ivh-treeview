57eb8b28b61ab2ed96f6aa8317a75818a57fda77
Justin <jus.russell@gmail.com>
2014-07-18 11:28:55 -0400

    test: Add test for initial selection support
    See #4
    
    [ci-skip]

--- test/spec/directives/ivh-treeview.js
+++ test/spec/directives/ivh-treeview.js
@@ -67,6 +67,13 @@ describe('Directive ivhTreeview', function() {
   describe('programmatic data changes', function() {
     var $el;
 
+    it('should honor initial selections', function() {
+      scope.bag1[0].children[1].selected = true;
+      $el = compile(tplBasic, scope);
+      expect(scope.bag1[0].children[1].children[0].selected).toBe(true);
+      expect(scope.bag1[0].__ivhTreeviewIntermediate).toBe(true);
+    });
+
     it('selected status changes should effect parent and child nodes', function() {
       $el = compile(tplBasic, scope);
       scope.bag1[0].children[1].selected = true;
