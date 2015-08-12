5a8a7e1f7d291d90f0edfee862c0a6024cccd0be
Justin <jus.russell@gmail.com>
2015-08-12 08:20:51 -0400

    feature: More copy and a demo

--- main.js
+++ main.js
@@ -13,18 +13,25 @@ ng.module('demo', ['ivh.treeview'])
   .controller('SillyCtrl', function(ivhTreeviewMgr) {
     this.stuff = [{
       label: 'Suitcase',
-      children: [{
-        label: 'Jacket'
-      }, {
-        label: 'Shoes'
-      }, {
-        label: 'Hat box',
-        selected: true,
-        children: [{
-          label: 'Fedora'
-        }]
-      }]
+      children: [
+        { label: 'Jacket' },
+        { label: 'Shoes' },
+        {
+          label: 'Hat box',
+          selected: true,
+          children: [
+            { label: 'Fedora' },
+            { label: 'Flat cap' },
+            { label: 'Top hat' },
+            { label: 'Five panel' },
+          ]
+        }
+      ]
     }];
+
+    this.tplFolderOpen = '<span class="twistie glyphicon glyphicon-folder-open"></span>';
+    this.tplFolderClose = '<span class="twistie glyphicon glyphicon-folder-close"></span>';
+    this.tplLeaf = '<span class="twistie glyphicon glyphicon-map-marker"></span>';
   });
 
 }(angular));
